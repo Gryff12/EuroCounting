253595104ecdc5caaf330a2c94b9385562ceec78
Nicolas Muller <nicolas.muller68125@gmail.com>
2023-10-23 23:33:58 +0200

    Added video handling & better detection

--- main.py
+++ main.py
@@ -2,21 +2,38 @@ import cv2
 import numpy as np
 from utils import *
 
+def empty(a):
+    pass
+def main() -> None:
+    # Capture vidéo depuis la caméra par défaut
+    cap = cv2.VideoCapture(0)
 
-def main():
-    image_path = 'images/test_image.jpg'
-    image = cv2.imread(image_path)
+    # Réglez la résolution de la caméra
+    cap.set(3, 640)
+    cap.set(4, 480)
 
-    if image is not None:
-        processed_image = img_pre_processing(image)
-        stacked_images = stack_images(image, processed_image)
+    cv2.namedWindow("Settings")
+    cv2.resizeWindow("Settings", 640,240)
+    cv2.createTrackbar("Threshold1", "Settings", 50, 255, empty)
+    cv2.createTrackbar("Threshold2", "Settings", 50, 255, empty)
 
-        # Affichez l'image superposée dans une fenêtre spécifiée
-        cv2.imshow('Euros', stacked_images)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    else:
-        print("L'image n'a pas pu être chargée.")
+    while True:
+        success, image = cap.read()
+
+        if success:
+            processed_image = img_pre_processing(image)
+            stacked_images = stack_images(image, processed_image)
+            cv2.imshow('Euros', stacked_images)
+
+            # Attendre 1 milliseconde et vérifier si l'utilisateur a appuyé sur la touche 'q' pour quitter
+            key = cv2.waitKey(1) & 0xFF  # 0xFF to support all versions of OpenCV
+            if key == ord('q'):
+                break
+        else:
+            print("L'image n'a pas pu être chargée.")
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -4,8 +4,16 @@ import numpy as np
 
 def img_pre_processing(img: np.ndarray) -> np.ndarray:
     img_blurred = cv2.GaussianBlur(img, (5, 5), 3)
-    img_canny = cv2.Canny(img_blurred, 150, 200)
-    return img_canny
+
+    threshold1 = cv2.getTrackbarPos("Threshold1", "Settings")
+    threshold2 = cv2.getTrackbarPos("Threshold2", "Settings")
+    img_canny = cv2.Canny(img_blurred, threshold1, threshold2)
+
+    kernel = np.ones((3, 3), np.uint8)
+
+    img_dilated = cv2.dilate(img_canny, kernel, iterations=1)
+    img_pre = cv2.morphologyEx(img_dilated,cv2.MORPH_CLOSE, kernel)
+    return img_pre
 
 
 def convert_to_bgr(image: np.ndarray) -> np.ndarray:
@@ -17,5 +25,3 @@ def convert_to_bgr(image: np.ndarray) -> np.ndarray:
 def stack_images(*images: np.ndarray) -> np.ndarray:
     images_bgr = [convert_to_bgr(image) for image in images]
     return np.hstack(images_bgr)
-
-
